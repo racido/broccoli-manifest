ab83b30fe7a6b1a61618efdb3b855fb14b8b12ba
Sjoerd de Jong <sjoerd@smwm.nl>
2014-06-23 23:18:42 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+var fs = require("fs");
+var path = require('path');
+var brocWriter = require("broccoli-writer");
+var helpers = require("broccoli-kitchen-sink-helpers");
+
+var BroccoliManifest = function BroccoliManifest(inTree, options) {
+  if (!(this instanceof BroccoliManifest)) {
+    return new BroccoliManifest(inTree, options);
+  }
+  this.inTree = inTree;
+  options = options || {};
+  this.appcacheFile = options.appcacheFile || "/manifest.appcache";
+};
+
+modules.exports = BroccoliManifest;
+BroccoliManifest.prototype = Object.create(brocWriter.prototype);
+BroccoliManifest.prototype.constructor = BroccoliManifest;
+BroccoliManifest.prototype.description = "Creates an manifest.appcache file for your project";
+
+BroccoliManifest.prototype.write = function(readTree, destDir) {
+  var appcacheFile = this.appcacheFile;
+  return readTree(this.inTree).then(function (srcDir) {
+    var lines = ["CACHE MANIFEST", "# created " + (new Date()).toISOString(), "", "CACHE:"];
+
+    getFilesRecursively(srcDir, [ "**/*" ]).forEach(function (file) {
+      var srcFile = path.join(srcDir, file);
+      var stat = fs.lstatSync(srcFile);
+
+      if (!stat.isFile() && !stat.isSymbolicLink())
+        return;
+
+      lines.push(srcFile);
+    });
+
+    lines.push("","NETWORK:","*");
+
+    fs.writeFileSync(path.join(destDir, appcacheFile), lines.join("\n"));
+  });
+};
+
+function getFilesRecursively(dir, globPatterns) {
+  return helpers.multiGlob(globPatterns, { cwd: dir });
+}
+
